36acad4a485135b7528db172bd648a3fdfd7186b
Skyler Herman <sah7829@rit.edu>
2023-11-25 13:28:45 -0500

    Should amplify fail, need something to fallback on

--- giraffe-gang/src/data/output.js
+++ giraffe-gang/src/data/output.js
@@ -1,3 +1,3 @@
-const URL = "https://shacxtz19h.execute-api.us-east-2.amazonaws.com/dev";
+const API_URL = "https://shacxtz19h.execute-api.us-east-2.amazonaws.com/dev";
 
-export default URL;
+export default API_URL;
