d3ebdf788538aeeb3960852bc3fe6d1c59975e63
= <esc2570@rit.edu>
2023-11-29 17:23:01 -0500

    still figuring out how to get the body

--- giraffe-gang/src/components/Reports.jsx
+++ giraffe-gang/src/components/Reports.jsx
@@ -18,7 +18,7 @@ function Reports() {
   useEffect(() => {
     console.log(isDemo)
     console.log(hourly_report_url)
-    console.log(reportData.body)
+    console.log(reportData)
     const handleHourlyReport = () => {
   
       fetch(hourly_report_url, {
