cd1da640a8f0101ebc8bc7390a2fedf304b1831f
Skyler Herman <skyler0herman@gmail.com>
2023-11-29 19:39:20 -0500

    Made the reports tables all nice

--- giraffe-gang/src/components/Reports.jsx
+++ giraffe-gang/src/components/Reports.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Table, Tag } from 'antd';
+import { Flex } from 'antd';
 
 import "../styles/Reports.css";
 
@@ -48,31 +48,28 @@ function Reports() {
       <h1 className="reports-heading">Reports</h1>
       {/* Add reports content here */}
       {reportData ? (
-        <div>{JSON.stringify(reportData)}
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Date</th>
-              <th>Giraffes</th>
-              <th>Confidence</th>
-              <th>Link</th>
-            </tr>
-          </thead>
-          <tbody>
-            {reportData.map((item,index)=>{
+        <div>
+          {/* {JSON.stringify(reportData)} */}
+          <Flex gap="middle" vertical>
+            <Flex gap="middle" horizontal>
+              <div style={{backgroundColor: '#DED0B6', width: '25%', padding: "1%",}}><b>ID</b></div>
+              <div style={{backgroundColor: '#DED0B6', width: '25%', padding: "1%",}}><b>Date</b></div>
+              <div style={{backgroundColor: '#DED0B6', width: '25%', padding: "1%",}}><b>Giraffes</b></div>
+              <div style={{backgroundColor: '#DED0B6', width: '25%', padding: "1%", }}><b>Confidence</b></div>
+              <div style={{backgroundColor: '#DED0B6', width: '25%', padding: "1%",}}><b>Link</b></div>
+            </Flex>
+            {JSON.parse(reportData.body).map((item,index)=>{
               return(
-                <tr>
-                  <td>{item[0]}</td>
-                  <td>{item[1]}</td>
-                  <td>{item[2]}</td>
-                  <td>{item[3]}</td>
-                  <td>{item[4]}</td>
-                </tr>
+                <Flex gap="middle" horizontal>
+                  <div key={index} style={{backgroundColor: index % 2 ? '#DED0B6' : '#FAEED1', width: '20%', padding: "1%",}} >{item[0]}</div>
+                  <div key={index} style={{backgroundColor: index % 2 ? '#DED0B6' : '#FAEED1', width: '20%', padding: "1%",}} >{item[1]}</div>
+                  <div key={index} style={{backgroundColor: index % 2 ? '#DED0B6' : '#FAEED1', width: '20%', padding: "1%",}} >{item[2]}</div>
+                  <div key={index} style={{backgroundColor: index % 2 ? '#DED0B6' : '#FAEED1', width: '20%', padding: "1%",}} >{item[3]}</div>
+                  <div key={index} style={{backgroundColor: index % 2 ? '#DED0B6' : '#FAEED1', width: '20%', padding: "1%",}} >{item[4]}</div>
+                </Flex>
               )
             })}
-          </tbody>
-        </table>
+          </Flex>
         </div>
       ) : (
         <p>Loading...</p>
